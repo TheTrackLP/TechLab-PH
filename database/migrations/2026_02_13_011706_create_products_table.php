805be990f3d12add3c19ecffc55944d6409d19a8
TheTrackLP <catigemmanuel@gmail.com>
2026-02-14 18:06:42 +0800

    Added supplier_id foreign key from Supplier Model

--- database/migrations/2026_02_13_011706_create_products_table.php
+++ database/migrations/2026_02_13_011706_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('category_id')->constrained();
+            $table->foreignId('supplier_id')->constrained();
             $table->string('brand')->nullable();
             $table->string('image')->nullable();
             $table->string('name');
