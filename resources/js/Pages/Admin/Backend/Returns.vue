9400c5e1a8bab2c29e1e838cc316cf6318312790
TheTrackLP <catigemmanuel@gmail.com>
2026-07-06 22:45:08 +0800

    Added v-models

--- resources/js/Pages/Admin/Backend/Returns.vue
+++ resources/js/Pages/Admin/Backend/Returns.vue
@@ -174,19 +174,37 @@ export default {
                     <div class="row mb-3">
                         <div class="col-md-4">
                             <strong>Return Reason:</strong>
-                            <select name="" id="" class="form-select">
-                                <option value=""></option>
+                            <select
+                                v-model="returnForm.returnReason"
+                                class="form-select"
+                            >
+                                <option>Select an Option</option>
+                                <option value="defective">Defective</option>
+                                <option value="wrong_item">Wrong Item</option>
+                                <option value="change_mind">
+                                    Customer Changed Mind
+                                </option>
+                                <option value="damaged">Damaged</option>
                             </select>
                         </div>
                         <div class="col-md-4">
                             <strong>Return Type:</strong>
-                            <select name="" id="" class="form-select">
-                                <option value=""></option>
+                            <select
+                                v-model="returnForm.returnType"
+                                class="form-select"
+                            >
+                                <option>Select on Option</option>
+                                <option class="refund">Refund</option>
+                                <option class="exchange">Exchange</option>
                             </select>
                         </div>
                         <div class="col-md-4">
                             <strong>Notes:</strong>
-                            <input type="text" class="form-control" />
+                            <input
+                                type="text"
+                                class="form-control"
+                                v-model="returnForm.returnNote"
+                            />
                         </div>
                     </div>
                 </div>
